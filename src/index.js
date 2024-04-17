745b8e2699db3ee618574e4ad6b5e0fb78114450
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-04-17 08:48:47 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza.Co</h1>;
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinachi"
+        ingredients="Tomato , mozarella , spinach and ricotto cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="20"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price="13"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props)
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  // return React.createElement('footer',null,"We're Currently open");
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We're Currently Open{" "}
+    </footer>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// React before 18
+// import ReactDOM from "react-dom"
+// ReactDOM.render(<App/>,document.querySelector('root'));
